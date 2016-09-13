f795382a4b4e0056facf2dbd4a9907255eeb00a9
Aayush Kapoor <xeoneux@icloud.com>
2016-09-13 14:19:38 +1000

    feat: rename login view controller

--- On the Map/LoginViewController.swift
+++ On the Map/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  On the Map
 //
 //  Created by Aayush Kapoor on 12/09/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     @IBOutlet weak var submitButton: UIButton!
 
