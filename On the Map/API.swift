cf0d8539609dbc303f321164e5b47c44933463ae
Aayush Kapoor <xeoneux@icloud.com>
2016-09-13 12:22:25 +1000

    feat: refactor api methods

--- On the Map/API.swift
+++ On the Map/API.swift
@@ -18,6 +18,38 @@ func Error(error: String, domain: String) -> NSError {
     return NSError(domain: domain, code: 0, userInfo: userInfo)
 }
 
+func Task(request: NSMutableURLRequest, handler: (result: AnyObject!, error: NSError?) -> Void) {
+    let session = NSURLSession.sharedSession()
+    let task = session.dataTaskWithRequest(request) { (data, response, error) in
+
+        guard error == nil else {
+            handler(result: nil, error: Error("Data task error", domain: "POST"))
+            return
+        }
+
+        guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+            handler(result: nil, error: Error("Response status code not 2xx", domain: "POST"))
+            return
+        }
+
+        guard data != nil else {
+            handler(result: nil, error: Error("No data recieved from the server", domain: "POST"))
+            return
+        }
+
+        do {
+            let parsedData = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as! [String: AnyObject]
+            handler(result: parsedData, error: nil)
+        } catch {
+            handler(result: nil, error: Error("Cannot parse JSON data", domain: "POST"))
+            return
+        }
+
+    }
+
+    task.resume()
+}
+
 class API {
 
     static func get(domain: Domain, handler: (result: AnyObject!, error: NSError?) -> Void) {
@@ -26,35 +58,7 @@ class API {
 
         request.HTTPMethod = "GET"
 
-        let session = NSURLSession.sharedSession()
-        let task = session.dataTaskWithRequest(request) { (data, response, error) in
-
-            guard error == nil else {
-                handler(result: nil, error: Error("Data task error", domain: "GET"))
-                return
-            }
-
-            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
-                handler(result: nil, error: Error("Response status code not 2xx", domain: "GET"))
-                return
-            }
-
-            guard data != nil else {
-                handler(result: nil, error: Error("No data recieved from the server", domain: "GET"))
-                return
-            }
-
-            do {
-                let parsedData = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as! [String: AnyObject]
-                handler(result: parsedData, error: nil)
-            } catch {
-                handler(result: nil, error: Error("Cannot parse JSON data", domain: "GET"))
-                return
-            }
-
-        }
-
-        task.resume()
+        Task(request, handler: handler)
     }
 
     static func post(domain: Domain, body: String, handler: (result: AnyObject!, error: NSError?) -> Void) {
@@ -67,34 +71,6 @@ class API {
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.HTTPBody = body.dataUsingEncoding(NSUTF8StringEncoding)
 
-        let session = NSURLSession.sharedSession()
-        let task = session.dataTaskWithRequest(request) { (data, response, error) in
-
-            guard error == nil else {
-                handler(result: nil, error: Error("Data task error", domain: "POST"))
-                return
-            }
-
-            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
-                handler(result: nil, error: Error("Response status code not 2xx", domain: "POST"))
-                return
-            }
-
-            guard data != nil else {
-                handler(result: nil, error: Error("No data recieved from the server", domain: "POST"))
-                return
-            }
-
-            do {
-                let parsedData = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as! [String: AnyObject]
-                handler(result: parsedData, error: nil)
-            } catch {
-                handler(result: nil, error: Error("Cannot parse JSON data", domain: "POST"))
-                return
-            }
-
-        }
-
-        task.resume()
+        Task(request, handler: handler)
     }
 }
