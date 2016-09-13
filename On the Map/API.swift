22cf0b965e4b28850a83835e64e1fbddf44a0d96
Aayush Kapoor <xeoneux@icloud.com>
2016-09-13 12:18:02 +1000

    feat: add post method

--- On the Map/API.swift
+++ On the Map/API.swift
@@ -56,4 +56,45 @@ class API {
 
         task.resume()
     }
+
+    static func post(domain: Domain, body: String, handler: (result: AnyObject!, error: NSError?) -> Void) {
+
+        let request = NSMutableURLRequest(URL: NSURL(string: domain.rawValue)!)
+
+        request.HTTPMethod = "POST"
+
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.HTTPBody = body.dataUsingEncoding(NSUTF8StringEncoding)
+
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+
+            guard error == nil else {
+                handler(result: nil, error: Error("Data task error", domain: "POST"))
+                return
+            }
+
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                handler(result: nil, error: Error("Response status code not 2xx", domain: "POST"))
+                return
+            }
+
+            guard data != nil else {
+                handler(result: nil, error: Error("No data recieved from the server", domain: "POST"))
+                return
+            }
+
+            do {
+                let parsedData = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) as! [String: AnyObject]
+                handler(result: parsedData, error: nil)
+            } catch {
+                handler(result: nil, error: Error("Cannot parse JSON data", domain: "POST"))
+                return
+            }
+
+        }
+
+        task.resume()
+    }
 }
