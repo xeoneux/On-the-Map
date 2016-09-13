c7d4e20027d67f40f50eb7e2db128c01ec5f7289
Aayush Kapoor <xeoneux@icloud.com>
2016-09-13 16:39:52 +1000

    add map view controller

---
+++ On the Map/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  On the Map
+//
+//  Created by Aayush Kapoor on 13/09/16.
+//  Copyright © 2016 Aayush Kapoor. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+
+}
