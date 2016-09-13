e33656eded9cd09093ea4e4521426c66a842cd09
Aayush Kapoor <xeoneux@icloud.com>
2016-09-13 16:43:28 +1000

    add list view controller

---
+++ On the Map/ListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ListViewController.swift
+//  On the Map
+//
+//  Created by Aayush Kapoor on 13/09/16.
+//  Copyright © 2016 Aayush Kapoor. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+    
+}
