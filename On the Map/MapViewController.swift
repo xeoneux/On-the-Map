1409efdb6e91328d09b9a3aef367785ef0d3f241
Aayush Kapoor <xeoneux@icloud.com>
2016-09-14 22:13:37 +1000

    feat: add map annotations

--- On the Map/MapViewController.swift
+++ On the Map/MapViewController.swift
@@ -12,6 +12,25 @@ import MapKit
 class MapViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
-    
 
+    override func viewDidLoad() {
+        let annotations: [MKPointAnnotation] = MapPin.getPins().map {
+            let latitude = CLLocationDegrees($0.latitude)
+            let longitude = CLLocationDegrees($0.longitude)
+            let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+
+            let firstName = $0.firstName
+            let lastName = $0.lastName
+            let title = "\(firstName) \(lastName)"
+
+            let annotation = MKPointAnnotation()
+            annotation.title = title
+            annotation.subtitle = $0.mediaUrl
+            annotation.coordinate = coordinate
+
+            return annotation
+        }
+
+        mapView.addAnnotations(annotations)
+    }
 }
